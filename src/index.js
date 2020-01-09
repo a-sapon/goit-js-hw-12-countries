0797a1349c84ea5206fddf7b13c59b7530bd8964
a-sapon <sapon.anya@gmail.com>
2020-01-09 15:13:31 +0200

    used lodash debounce

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import './styles.css';
 import fetchCountries from './fetchCountries';
 import templateOneCountry from './templates/country.hbs';
 import PNotify from '../node_modules/pnotify/dist/es/PNotify.js';
-import "../node_modules/pnotify/dist/PNotifyBrightTheme.css";
+import '../node_modules/pnotify/dist/PNotifyBrightTheme.css';
+var debounce = require("lodash.debounce");
 
 const refs = {
   searchForm: document.getElementById('search-form'),
@@ -10,48 +11,44 @@ const refs = {
   listOutput: document.querySelector('.list-output'),
 };
 
-refs.searchForm.addEventListener('input', inputHandler);
-let timerId;
-
-function inputHandler(e) {
+const delay = debounce(function inputHandler() {
   clearOutput();
   PNotify.closeAll();
-  clearTimeout(timerId);
-  const searchValue = e.currentTarget.elements.search.value;
-  timerId = setTimeout(() => {
-    if (searchValue === '') {
-      clearOutput();
-      return;
-    }
-    fetchCountries(searchValue)
-      .then(data => {
-       if (data && data.status === 404){
-        PNotify.notice('There\s no such country :( Please check your spelling!');
-       };
-        if (data.length > 10) {
-          PNotify.error({
-            text: 'Too many matches found. Please enter a more specisic query!',
-          });
-          clearOutput();
-        } else if (data.length > 1 && data.length < 10) {
-          data.map(country => {
-            refs.listOutput.insertAdjacentHTML(
-              'beforeend',
-              `<li>${country.name}</li>`,
-            );
-          });
-          refs.output.innerHTML = '';
-          PNotify.closeAll();
-        } else if (data.length === 1) {
-          const completedTemplate = templateOneCountry(data);
-          refs.output.insertAdjacentHTML('beforeend', completedTemplate);
-          refs.listOutput.innerHTML = '';
-          PNotify.closeAll();
-        }
-      })
-      .catch(error => console.log('My error: ', error));
-  }, 500);
-}
+  const searchValue = refs.searchForm.elements.search.value;
+  if (searchValue === '') {
+    clearOutput();
+    return;
+  }
+  fetchCountries(searchValue)
+    .then(data => {
+      if (data && data.status === 404) {
+        PNotify.notice('Theres no such country :( Please check your spelling!');
+      }
+      if (data.length > 10) {
+        PNotify.error({
+          text: 'Too many matches found. Please enter a more specisic query!',
+        });
+        clearOutput();
+      } else if (data.length > 1 && data.length < 10) {
+        data.map(country => {
+          refs.listOutput.insertAdjacentHTML(
+            'beforeend',
+            `<li>${country.name}</li>`,
+          );
+        });
+        refs.output.innerHTML = '';
+        PNotify.closeAll();
+      } else if (data.length === 1) {
+        const completedTemplate = templateOneCountry(data);
+        refs.output.insertAdjacentHTML('beforeend', completedTemplate);
+        refs.listOutput.innerHTML = '';
+        PNotify.closeAll();
+      }
+    })
+    .catch(error => console.log('My error: ', error))
+}, 500)
+
+refs.searchForm.addEventListener('input', delay);
 
 function clearOutput() {
   refs.listOutput.innerHTML = '';
